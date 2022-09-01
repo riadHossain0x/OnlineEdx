6df134f78c3d6abaf087d851be54c584f5505e25
Riad <riadofficial.me@gmail.com>
2022-09-01 07:22:54 +0600

    #1 Initial setup

---
+++ src/OnlineEdx/Tests/Online.Edx.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Online.Edx.Infrastructure.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
